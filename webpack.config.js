7607715c3a6bfb7b3ab6d2763d2a945b2b752e15
Rafael Ruiz <r.ruiz@cathedralsw.com>
2025-01-31 12:20:49 +0100

    update .gitignore and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './game.js',
@@ -21,19 +22,21 @@ module.exports = {
         exclude: /node_modules/,
         use: 'ts-loader',
       },
-      {
-        test: /\.png$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'images/',
-          },
-        },
-      },
     ],
   },
   resolve: {
     extensions: ['.js', '.ts'],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'index.html', to: 'index.html' },
+        { from: 'assets/favicon.ico', to: 'favicon.ico' },
+        { from: 'assets/hex-pack/PNG/Objects/', to: 'assets/hex-pack/PNG/Objects/' },
+        { from: 'assets/hex-pack/PNG/Tiles/Terrain/Grass/', to: 'assets/hex-pack/PNG/Tiles/Terrain/Grass/' },
+        { from: 'assets/icons/', to: 'assets/icons/' },
+        { from: 'assets/music/', to: 'assets/music/' },
+      ],
+    }),
+  ],
 };
