6ac066ace6b8be2d90ea948e5382759ee443844a
Chuwi <quitiweb.chuwi@gmail.com>
2026-04-26 01:54:55 +0200

    Añade Pixel Drift como cuarto juego

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
         { from: 'factory-world.html', to: 'factory-world.html' },
         { from: 'tic-tac-toe.html', to: 'tic-tac-toe.html' },
         { from: 'dungeon-sprint.html', to: 'dungeon-sprint.html' },
+        { from: 'pixel-drift.html', to: 'pixel-drift.html' },
         { from: 'assets/favicon.ico', to: 'favicon.ico' },
         { from: 'assets/covers/', to: 'assets/covers/' },
         { from: 'assets/hex-pack/PNG/Objects/', to: 'assets/hex-pack/PNG/Objects/' },
