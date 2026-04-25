78b07531136de5e47fb6bca8a7e30a7cdda65821
Chuwi <quitiweb.chuwi@gmail.com>
2026-04-26 01:29:23 +0200

    Añade Dungeon Sprint como tercer juego

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
         { from: 'index.html', to: 'index.html' },
         { from: 'factory-world.html', to: 'factory-world.html' },
         { from: 'tic-tac-toe.html', to: 'tic-tac-toe.html' },
+        { from: 'dungeon-sprint.html', to: 'dungeon-sprint.html' },
         { from: 'assets/favicon.ico', to: 'favicon.ico' },
         { from: 'assets/covers/', to: 'assets/covers/' },
         { from: 'assets/hex-pack/PNG/Objects/', to: 'assets/hex-pack/PNG/Objects/' },
